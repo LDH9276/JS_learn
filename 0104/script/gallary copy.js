d80b972f5e7308ea9dc205e468c9e91591126f7d
LDH9276 <leedh9276@naver.com>
2023-01-06 12:30:43 +0900

    ver 1.1 update by jan.6

---
+++ 0104/script/gallary copy.js
@@ -0,0 +1,44 @@
+let smallImg = document.querySelectorAll('.thumb');
+let bigImg = document.querySelector('#photo');
+
+for (let i=0; i<smallImg.length; i++){
+  smallImg[i].addEventListener('click', changeImg);
+}
+
+function changeImg(){
+  let imgUrl = this.src;
+  bigImg.setAttribute('src', imgUrl)
+
+  for (let j=0; j<smallImg.length; j++){
+    smallImg[j].style.border="none";
+  }
+  this.style.border="4px solid orangered"
+
+}
+
+    //증가하는 함수
+  let prev = document.getElementById('prev');
+  let next = document.getElementById('next')
+  let n=1;
+
+  prev.addEventListener('click', minus);
+  next.addEventListener('click', plus);
+
+  function plus(){
+    if(n==9){
+        n = 1;
+      }else{
+        n++;
+      }
+      document.getElementById('photo').src="./img/movie_image0"+n+".jpeg"
+    };
+
+    //감소하는 함수
+    function minus(){
+      if(n==1){
+        n = 9;
+      }else{
+        n--;
+      }
+      document.getElementById('photo').src="./img/movie_image0"+n+".jpeg"
+    }
