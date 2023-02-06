b9c60bbb6a3702b2b463ea683c62070a91fc2798
LDH9276 <leedh9276@naver.com>
2023-02-06 15:31:09 +0900

    ver 1.18

--- 0202/casper.js
+++ 0202/casper.js
@@ -6,7 +6,7 @@ const next_btn = document.getElementById('slide-next_btn');
 const page_btn = document.querySelectorAll('.e-pagenation span');
 const img_n = slide_img.length;
 
-const img_w = 1000;
+const img_w = 900;
 const m = 0; 
 const s_con = 1;
 let count = 0;
@@ -19,6 +19,8 @@ function mslide(n){
   }
   page_btn[n].classList.add('on');
 }
+
+slideEvent(0);
 
 prev_btn.addEventListener('click', function(){ 
   if(count > 0){
@@ -65,37 +67,4 @@ for(let i=0; i<page_btn.length; i++){
         mslide(0);
       }} , 3000);
     });
-
-$(document).ready(function(){
-    let modal = `
-      <div class="modal">
-        <div class="m_inner">
-          <img src="images/pc_prod_notice_20211210.jpg" alt="modal image">
-          <p><input type="checkbox" id="ch"><label for="ch">일주일간 열지 않음</label>
-          <a href="#" title="닫기">닫기</a></p>
-        </div>
-      </div>
-    `
-
-      $('body').append(modal);
-      if($.cookie('popup') == 'none'){
-        $('.modal').hide();
-      } 
-      let ch = $('.modal #ch');
-
-    //쿠키파일이 현재 브라우저에 존재한다면 모달창이 안나오도록 한다.
-    
-
-    //#ch의 체크 여부로 쿠키 생성하거나 모달을 숨긴다.
-    function closeModal(){
-      if(ch.is(':checked')){
-        $.cookie('popup', 'none', {expires:7, path:'/'});
-      }
-      $('.modal').hide();
-    };
-
-    //닫기 버튼을 클릭하여 closeModal 함수를 동작하게 한다.
-    $('.modal a').click(function(){
-      closeModal();
-    });
-  });
+  
