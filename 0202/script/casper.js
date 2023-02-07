b8f8ae3c2c31147118908a5cb6f619472159c75b
LDH9276 <leedh9276@naver.com>
2023-02-07 13:54:37 +0900

    ver 1.17

--- 0202/script/casper.js
+++ 0202/script/casper.js
@@ -1,16 +1,33 @@
-//1. 변수
+let swiper = new Swiper('.top3', {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  }
+});
+
+
+// 이벤트 슬라이드
 const slide = document.querySelector('.e-slide');
 const slide_img = document.querySelectorAll('.slide-item');
 const prev_btn = document.getElementById('slide-prev_btn');
 const next_btn = document.getElementById('slide-next_btn');
 const page_btn = document.querySelectorAll('.e-pagenation span');
 const img_n = slide_img.length;
-
 const img_w = 1000;
 const m = 0; 
 const s_con = 1;
 let count = 0;
 
+//슬라이드 이동 함수
 function mslide(n){
   slide.style.left=(img_w+m)*-n+'px';
   count = n;
@@ -20,6 +37,7 @@ function mslide(n){
   page_btn[n].classList.add('on');
 }
 
+//이전 버튼 클릭시
 prev_btn.addEventListener('click', function(){ 
   if(count > 0){
     mslide(count-1);
@@ -29,6 +47,7 @@ prev_btn.addEventListener('click', function(){
   clearInterval(Timer);
 });
 
+//다음 버튼 클릭시
 next_btn.addEventListener('click', function(){
   if(count < img_n-s_con){
     mslide(count+1);
@@ -38,6 +57,7 @@ next_btn.addEventListener('click', function(){
   clearInterval(Timer);
 });
 
+//페이지 버튼 클릭시
 for(let i=0; i<page_btn.length; i++){
   page_btn[i].addEventListener('click', function(){
     mslide(i);
@@ -47,26 +67,30 @@ for(let i=0; i<page_btn.length; i++){
     page_btn[i].classList.add('on');
   });
 }
-  let Timer = setInterval(function(){
+
+//자동 슬라이드
+let Timer = setInterval(function(){
+  if(count < img_n-s_con){
+    mslide(count+1);
+  }else{
+    mslide(0);
+  }}, 3000);
+
+//마우스 오버시 슬라이드 멈춤
+slide.addEventListener('mouseenter', function(){
+  clearInterval(Timer);
+});
+slide.addEventListener('mouseleave', function(){
+  Timer = setInterval(function(){
     if(count < img_n-s_con){
       mslide(count+1);
     }else{
       mslide(0);
-    }}, 3000);
+    }} , 3000);
+  });
 
-  slide.addEventListener('mouseenter', function(){
-    clearInterval(Timer);});
-
-  slide.addEventListener('mouseleave', function(){
-    Timer = setInterval(function(){
-      if(count < img_n-s_con){
-        mslide(count+1);
-      }else{
-        mslide(0);
-      }} , 3000);
-    });
-
-$(document).ready(function(){
+// 모달창
+$(function(){
     let modal = `
       <div class="modal">
         <div class="m_inner">
@@ -98,4 +122,16 @@ $(document).ready(function(){
     $('.modal a').click(function(){
       closeModal();
     });
+
+    //랜덤번호 호출
+    let ranNum = Math.floor(Math.random()*3)+1;
+    let ranImg = document.querySelector('.ran_banner img');
+    ranImg.src = `./img/banner0${ranNum}.png`;
+
+    //랜덤 배너 (제이쿼리)
+    // $(function(){
+    //   let ranNum = Math.floor(Math.random()*3)+1;
+    //   $('.ran_banner img').attr('src', `./img/banner0${ranNum}.png`);
+    // })
+
   });
